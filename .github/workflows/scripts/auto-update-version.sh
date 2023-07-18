5788ee62354664e3c98453975809b599bc8ddbd3
tomokisun <github@tomokisun.com>
2023-07-18 18:20:55 +0900

    Update MARKETING_VERSION to current date

--- .github/workflows/scripts/auto-update-version.sh
+++ .github/workflows/scripts/auto-update-version.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# 変更する MARKETING_VERSION の値
+# Value to change MARKETING_VERSION to
 NEW_VERSION=$(date '+%y').$(date '+%m').$(date '+%d')
 
-# project.pbxproj ファイルのパス
+# Path to the project.pbxproj file
 PDXPROJ_FILE="BlackjackCounting.xcodeproj/project.pbxproj"
 
-# MARKETING_VERSION の行を探すパターン
+# Pattern to find the MARKETING_VERSION line
 VERSION_PATTERN="MARKETING_VERSION ="
 
-# MARKETING_VERSION の行を置き換えるための文字列
+# String for replacing the MARKETING_VERSION line
 REPLACEMENT="MARKETING_VERSION = $NEW_VERSION;"
 
-# ファイル内の MARKETING_VERSION を書き換える
+# Replace MARKETING_VERSION in the file
 sed -i '' "s/$VERSION_PATTERN.*/$REPLACEMENT/g" "$PDXPROJ_FILE"
 
-echo "MARKETING_VERSION が $NEW_VERSION に書き換えられました。"
+echo "MARKETING_VERSION has been changed to $NEW_VERSION."
